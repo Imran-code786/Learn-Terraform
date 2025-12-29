b897175ce04a780380cb865ffa8ef1ce866686b1
Imran <imrankhanmohammed130@gmail.com>
2025-12-29 11:38:45 -0600

    dfd

--- vars.tf
+++ vars.tf
@@ -77,9 +77,9 @@ output "course"{
 }
 
 output "courses"{
-    value = var.courses
+    value = var.courses[2]
 }
 
 output "course_details"{
-    value = var.course_details
+    value = var.course_details["DevOps"]
 }
