3465c0eb6aea4372a0e53f6f3458623885c4f0b0
Imran <imrankhanmohammed130@gmail.com>
2025-12-30 15:34:16 -0600

    df

--- 06.modules/ec2/main.tf
+++ 06.modules/ec2/main.tf
@@ -1,51 +1,41 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
-    Name = "Hello world"
+    Name = "sample"
   }
 }
 
 data "aws_ami" "example" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["950538586636"]
-
-}
-
-resource "aws_security_group" "sg {
-name = "var.name"
-description = "Allow TLS inbound traffic"
-
-
-# Ingress Rules (Inbound Traffic)
-ingress {
-description = "SSH"
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+  owners      = ["950538586636"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
 }
 
 
+resource "aws_security_group" "sg" {
+  name        = "sample"
+  description = "Allow TLS inbound traffic"
 
-# Egress Rules (Outbound Traffic)
-egress {
-from_port = 0
-to_port = 0
-protocol = "-1" # Allow all protocols
-cidr_blocks = ["0.0.0.0/0"]
-ipv6_cidr_blocks = ["::/0"]
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sample"
+  }
 }
-
-tags = {
-Name = "var.name"
-}
-}
-
-variable "name"{}
-
-
-
--- 06.modules/main.tf
+++ 06.modules/main.tf
@@ -1,9 +1,5 @@
-module "sample1" {
+module "sample" {
     source = "./ec2"
     name = "sample1"
 }
 
-module "sample2 {
-    source = "./ec2"
-    name = "sample2"
-}
