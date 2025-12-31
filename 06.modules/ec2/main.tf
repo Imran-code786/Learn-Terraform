ab63128530bfff5b6cb722874ab12f05bfe3c2e7
Imran <imrankhanmohammed130@gmail.com>
2025-12-31 12:06:49 -0600

    wdsd

--- 06.modules/ec2/main.tf
+++ 06.modules/ec2/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "example" {
 
 
 resource "aws_security_group" "sg" {
-  name        = "sample"
+  name        = "sample1"
   description = "Allow TLS inbound traffic"
 
   ingress {
