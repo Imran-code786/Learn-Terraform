832b8fc75ac5dd3515829cdc9049d2a195f7ea3b
Imran <imrankhanmohammed130@gmail.com>
2026-01-01 15:55:55 -0600

    lk

--- 08.provisioner/main.tf
+++ 08.provisioner/main.tf
@@ -16,6 +16,11 @@ provisioner "remote-exec" {
         host = self.public_ip
     }
     inline = [
+      "sudo yum install -y nginx || sudo dnf install -y nginx",
+    "sudo systemctl enable nginx",
+    "sudo systemctl start nginx || true",
+    "sleep 5",
+    "sudo systemctl status nginx --no-pager",
       "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/Imran-code786/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
     ]
