47f994f4338e6a0ae0d9af795731faa2930699ee
Imran <imrankhanmohammed130@gmail.com>
2026-01-01 14:55:03 -0600

    efded

--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -9,3 +9,12 @@ module "sample2" {
     name = "sample"
     
 }
+
+output "public_ip_sample1" {
+    value = module.sample1.public_ip
+}
+
+
+output "public_ip_sample2" {
+    value = module.sample2.public_ip
+}
